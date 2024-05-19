380cd8246783569d5582a9d5e8d5a69dc26361f3
Chamika Jayarathna <failgames.lk@gmail.com>
2024-05-19 11:47:13 +0530

    modify header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,86 +1,74 @@
-import {Button as FlowbiteButton, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'; 
+import InputGroup from 'react-bootstrap/InputGroup'; 
 import { Link, useLocation } from 'react-router-dom'
 import styled from 'styled-components';
-import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
-
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const Logo = styled.span`
     background: linear-gradient(to right, #6366f1, #a855f7, #ec4899);   
 `;
 
-const Btn1 = styled(FlowbiteButton)`
-    width: 3rem;
-    height: 2.5rem;
+const StyledNavbar = styled(Navbar)`
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 `;
 
-const Btn2 = styled(FlowbiteButton)`
-    width: 3rem;
-    height: 2.5rem;
+const CenteredNav = styled(Nav)`
+    margin: 0 auto;
 `;
-
-const Btn3 = styled(FlowbiteButton)`
-    background: linear-gradient(to right, purple, blue); 
-    color: white; 
-    border: none;
-    /* width: 3rem;
-    height: 2.5rem; */
-`;
-
 
 const Header = () => {
+    const location = useLocation();
 
-    const path = useLocation.pathname;
+    return (
+        <StyledNavbar className="p-2" bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand>
+                    <Link to="/" className="align-self-center text-nowrap fs-6 fw-semibold">
+                        <Logo className="px-2 py-1 rounded-lg text-white">Sahand's</Logo>
+                        Blog
+                    </Link>
+                </Navbar.Brand>
 
-  return (
-    <Navbar className='border p-3'>
-        <Link to="/" className='align-self-center text-nowrap fs-6 fw-semibold'>
-            <Logo className='px-2 py-1 rounded-lg text-white'>Sahand's</Logo>
-            Blog
-        </Link>
+                <Form className="d-flex ms-auto me-3">
+                    <Form.Group className="mb-0">
+                        <InputGroup>
+                            <Form.Control
+                                type="search"
+                                placeholder="Search..."
+                                aria-label="Search"
+                            />
+                            <Button className="outline-secondary"><AiOutlineSearch /></Button>
+                        </InputGroup>
+                    </Form.Group>
+                </Form>
 
-        <form>
-            <TextInput
-                type='text'
-                placeholder='Search...'
-                rightIcon={AiOutlineSearch}
-                className='d-none d-lg-inline'
-            />
-        </form>
+                <CenteredNav className="me-auto">
+                    <Nav.Link as="div" active={location === "/"}>
+                        <Link to='/' className="nav-link">Home</Link>
+                    </Nav.Link>
+                    <Nav.Link as="div" active={location === "/about"}>
+                        <Link to='/about' className="nav-link">About</Link>
+                    </Nav.Link>
+                    <Nav.Link as="div" active={location === "/projects"}>
+                        <Link to='/projects' className="nav-link">Projects</Link>
+                    </Nav.Link>
+                </CenteredNav>
 
-        <Btn1 className='btn btn-secondary rounded-pill d-block d-lg-none'>
-            <AiOutlineSearch/>
-        </Btn1>
-        
-        {/* order-md-2 is not work */}
-        <div className="d-flex flex-row gap-2 order-md-2">
-            {/* dark mood button(not work) */}
-            <Btn2 className='btn btn-secondary rounded-pill d-none d-sm-inline'>
-                <FaMoon/>
-            </Btn2>
-            <Link to='/sign-in'>
-                <Btn3 className='btn btn-outline-primary'>
-                    Sign In
-                </Btn3>
-            </Link>
-            <Navbar.Toggle/>
-        </div>
-        <Navbar.Collapse>
-            <Navbar.Link active={path === "/"} as={'div'}>
-                <Link to='/'>Home</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === "/about"} as={'div'}>
-                <Link to='/about'>About</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === "/projects"} as={'div'}>
-                <Link to='/projects'>Projects</Link>
-            </Navbar.Link>
-        </Navbar.Collapse>
+                <Button variant="outline-info" className="ms-3"><FaMoon /></Button>
 
+                <Link to='/sign-in' className="ms-3">
+                    <Button className='btn btn-outline-primary'>Sign In</Button>
+                </Link>
 
-    </Navbar>
-  )
+            </Container>
+        </StyledNavbar>
+    );
 }
 
-export default Header
+export default Header;
