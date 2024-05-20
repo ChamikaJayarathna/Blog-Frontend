508b6e8241672e8fb7d36a1487b577545f18a02c
Chamika Jayarathna <failgames.lk@gmail.com>
2024-05-20 19:15:53 +0530

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,87 +1,70 @@
-import React from 'react';
-import { Container, Nav, Navbar, Form, Button, InputGroup } from 'react-bootstrap';
-import { Link, useLocation } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap'
 import styled from 'styled-components';
-import { FaMoon } from 'react-icons/fa';
-import { AiOutlineSearch } from 'react-icons/ai';
+import { FaSearch, FaMoon } from 'react-icons/fa';
 
-const Logo = styled.span`
-    background: linear-gradient(to right, #6366f1, #a855f7, #ec4899);  
-    border-radius: 8px;
-`;
 
 const StyledNavbar = styled(Navbar)`
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-bottom: 2px solid #dee2e6;
+  height: 75px;
 `;
 
-const CenteredNav = styled(Nav)`
-    margin: 0 auto;
+const Logo = styled(Link)`
+
+    text-decoration: none;
+    color: black;
+    margin-left: 4rem;
+    font-size: 1rem;
 `;
 
-const SignInButton = styled(Button)`
-    background-image: linear-gradient(to right, purple, blue);
+const LogoName = styled.span`
+    background: linear-gradient(to right, #6366f1, #a855f7, #ec4899);  
+    border-radius: 9px;
+    padding: .5rem 1rem;
     color: white;
-    border: 2px solid white;
-
-    &:hover{
-        background-image: linear-gradient(to right, blue, purple);
-        color: white;
-        border: 2px solid white;
-    }
 `;
-
 
 const Header = () => {
-    const location = useLocation();
+  return (
+    <StyledNavbar>
+        <Logo to='/' className='align-self-center text-nowrap fw-bolder'>
+            <LogoName>Sahand's</LogoName>Blog
+        </Logo>
 
-    return (
-        <StyledNavbar className="navbar navbar-expand-lg navbar-light bg-body-tertiary p-2" bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand>
-                    <Link to="/" className="align-self-center text-nowrap fs-6 fw-semibold">
-                        <Logo className="px-2 py-1 rounded-lg text-white">Sahand's</Logo>
-                        Blog
-                    </Link>
-                </Navbar.Brand>
+        <form className='d-none d-lg-inline'>
+            <input type="text" placeholder="search course..." />
+            <button><FaSearch/></button>
+        </form>
 
-                <Form className="d-flex ms-auto me-3">
-                    <Form.Group className="mb-0">
-                        <InputGroup>
-                            <Form.Control
-                                type="search"
-                                placeholder="Search..."
-                                aria-label="Search"
-                            />
-                            <Button variant="outline-secondary"><AiOutlineSearch /></Button>
-                        </InputGroup>
-                    </Form.Group>
-                </Form>
+        <button className='d-lg-none rounded'><FaSearch/></button>
 
+        <div className='d-flex gap-3 flex-md-row-reverse'>
 
-                <Button variant="outline-info" className="ms-3"><FaMoon/></Button>
+            <div className="">
+                <button className='d-none d-sm-inline'><FaMoon/></button>
 
-                <Link to='/sign-in' className="btn btn-custom">
-                    <SignInButton className='btn btn-outline-primary'>Sign In</SignInButton>
+                <Link to='/sign-in'>
+                    <button>Sign In</button>
                 </Link>
+            </div>
+            
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to='/' className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to='/about' className="nav-link">About</Link>
+                </li>   
+                <li className="nav-item">
+                    <Link to='/projects' className="nav-link">Projects</Link>
+                </li>   
+            </ul>
 
-                <Navbar.Toggle aria-controls="navbarButtonsExample"/>
-                <Navbar.Collapse id="navbarButtonsExample">
-                    <CenteredNav className="me-auto mb-2 mb-lg-0">
-                        <Nav.Link as="div" active={location.pathname === "/"}>
-                            <Link to='/' className="nav-link">Home</Link>
-                        </Nav.Link>
-                        <Nav.Link as="div" active={location.pathname === "/about"}>
-                            <Link to='/about' className="nav-link">About</Link>
-                        </Nav.Link>
-                        <Nav.Link as="div" active={location.pathname === "/projects"}>
-                            <Link to='/projects' className="nav-link">Projects</Link>
-                        </Nav.Link>
-                    </CenteredNav>
-                </Navbar.Collapse>
-            </Container>
+        </div>
 
-        </StyledNavbar>
-    );
+    </StyledNavbar>
+  )
 }
 
 export default Header;
