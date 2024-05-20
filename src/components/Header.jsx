f174c37c827b3c833abd5e71ecd2ecaff5303110
Chamika Jayarathna <failgames.lk@gmail.com>
2024-05-20 10:44:35 +0530

    complete signin page functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,6 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button'; 
-import InputGroup from 'react-bootstrap/InputGroup'; 
-import { Link, useLocation } from 'react-router-dom'
+import React from 'react';
+import { Container, Nav, Navbar, Form, Button, InputGroup } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import { FaMoon } from 'react-icons/fa';
 import { AiOutlineSearch } from 'react-icons/ai';
@@ -23,11 +18,24 @@ const CenteredNav = styled(Nav)`
     margin: 0 auto;
 `;
 
+const SignInButton = styled(Button)`
+    background-image: linear-gradient(to right, purple, blue);
+    color: white;
+    border: 2px solid white;
+
+    &:hover{
+        background-image: linear-gradient(to right, blue, purple);
+        color: white;
+        border: 2px solid white;
+    }
+`;
+
+
 const Header = () => {
     const location = useLocation();
 
     return (
-        <StyledNavbar className="p-2" bg="light" expand="lg">
+        <StyledNavbar className="navbar navbar-expand-lg navbar-light bg-body-tertiary p-2" bg="light" expand="lg">
             <Container>
                 <Navbar.Brand>
                     <Link to="/" className="align-self-center text-nowrap fs-6 fw-semibold">
@@ -44,30 +52,34 @@ const Header = () => {
                                 placeholder="Search..."
                                 aria-label="Search"
                             />
-                            <Button className="outline-secondary"><AiOutlineSearch /></Button>
+                            <Button variant="outline-secondary"><AiOutlineSearch /></Button>
                         </InputGroup>
                     </Form.Group>
                 </Form>
 
-                <CenteredNav className="me-auto">
-                    <Nav.Link as="div" active={location === "/"}>
-                        <Link to='/' className="nav-link">Home</Link>
-                    </Nav.Link>
-                    <Nav.Link as="div" active={location === "/about"}>
-                        <Link to='/about' className="nav-link">About</Link>
-                    </Nav.Link>
-                    <Nav.Link as="div" active={location === "/projects"}>
-                        <Link to='/projects' className="nav-link">Projects</Link>
-                    </Nav.Link>
-                </CenteredNav>
 
-                <Button variant="outline-info" className="ms-3"><FaMoon /></Button>
+                <Button variant="outline-info" className="ms-3"><FaMoon/></Button>
 
-                <Link to='/sign-in' className="ms-3">
-                    <Button className='btn btn-outline-primary'>Sign In</Button>
+                <Link to='/sign-in' className="btn btn-custom">
+                    <SignInButton className='btn btn-outline-primary'>Sign In</SignInButton>
                 </Link>
 
+                <Navbar.Toggle aria-controls="navbarButtonsExample"/>
+                <Navbar.Collapse id="navbarButtonsExample">
+                    <CenteredNav className="me-auto mb-2 mb-lg-0">
+                        <Nav.Link as="div" active={location.pathname === "/"}>
+                            <Link to='/' className="nav-link">Home</Link>
+                        </Nav.Link>
+                        <Nav.Link as="div" active={location.pathname === "/about"}>
+                            <Link to='/about' className="nav-link">About</Link>
+                        </Nav.Link>
+                        <Nav.Link as="div" active={location.pathname === "/projects"}>
+                            <Link to='/projects' className="nav-link">Projects</Link>
+                        </Nav.Link>
+                    </CenteredNav>
+                </Navbar.Collapse>
             </Container>
+
         </StyledNavbar>
     );
 }
