7d987d57d515694f1c5f33e876182b4383df9750
Chamika Jayarathna <failgames.lk@gmail.com>
2024-07-20 20:48:20 +0530

    update new header 0.0.1

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+const Imgdiv = styled.div`
+  width: 8rem; 
+  height: 8rem;
+  align-self: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  justify-content: center; 
+  flex-direction: column;
+`;
 
 const DashProfile = () => {
+
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div>DashProfile</div>
+    <div>
+      <h1>profile</h1>
+      <Form className=''>
+        <Imgdiv className=''>
+          <img src={currentUser.profilePicture} alt="user" className='rounded-circle w-100 h-100 border border-5 object-fit-cover'/>
+        </Imgdiv>
+        
+      </Form>
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,163 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import { Dropdown, Navbar, Image } from 'react-bootstrap'
+import { Dropdown, Image } from 'react-bootstrap'
 import styled from 'styled-components';
-import { FaSearch, FaMoon, FaSun } from 'react-icons/fa';
+import { FaSearch, FaMoon, FaSun, FaBars } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 
-
-const StyledNavbar = styled(Navbar)`
-  border-bottom: 2px solid #dee2e6;
-  height: 75px;
+const HeaderContainer = styled.header`
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 `;
 
-const Logo = styled(Link)`
+const NavBar = styled.nav`
+    background-color: ${({ theme }) => theme === 'dark' ? '#0F172A' : '#E4ECFA'};
+    color: #4D61D1;
+    height: 90px;
+    box-shadow: 0px 2px 9px -5px rgba(0,0,0,0.75);
+`;
 
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    max-width: 1500px;
+    margin: 0 auto;
+    padding: 0 30px;
+
+    .logo{
+        font-size: x-large;
+        font-weight: bold;
+    }
+
+    ul{
+        list-style-type: none;
+    }
+
+    .nav{
+        display: flex;
+        align-items: center;
+
+        @media (max-width: 640px) {
+            display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+            position: absolute;
+            top: 90px;
+            left: 0;
+            width: 100%;
+            background-color: ${({ theme }) => theme === 'dark' ? '#0F172A' : '#E4ECFA'};
+            box-shadow: 0px 2px 9px -5px rgba(0,0,0,0.75);
+        }
+    }
+
+    .nav li{
+        margin-left: 10px;
+
+        @media (max-width: 640px) {
+            padding: 10px 0;
+            text-align: center;
+            margin-left: 0;
+        }
+    }
+
+    .search-box{
+        display: flex;
+        align-items: center;
+
+        /* @media (max-width: 640px) {
+            display: none;
+        } */
+    }
+
+    .hamburger{
+        display: none;
+        font-size: 25px;
+        cursor: pointer;
+        color: ${({ theme }) => theme === 'dark' ? '#FFFFFF' : '#8A8694'};
+
+        @media (max-width: 640px) {
+            display: block;
+        }
+    }
+
+    button{
+        padding: 8px;
+        border: none;
+        background: none;
+    }
+`;
+
+const StyledLink = styled(Link)`
+    color: ${({ theme }) => theme === 'dark' ? '#FFFFFF' : '#8A8694'};
     text-decoration: none;
-    color: black;
-    margin-left: 4rem;
-    font-size: 1rem;
+    font-size: 16px;
+    font-weight: bold;
+
+    &:hover{
+        color: #4D61D1;
+    }
 `;
 
-const LogoName = styled.span`
-    background: linear-gradient(to right, #6366f1, #a855f7, #ec4899);  
-    border-radius: 9px;
-    padding: .5rem 1rem;
-    color: white;
+const InputWrapper = styled.div`
+    position: relative;
+    width: 100%;
+
+    .search{
+        width: 100%;
+        padding: 9px 40px;
+        border: none;
+        border-radius: 5px;
+    }
+
+    button{
+        position: absolute;
+        color: #8A8694;
+        right: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
 `;
 
-const Blog = styled.span`
-  color: ${({ theme }) => theme === 'dark' ? '#FFFFFF' : 'black'};
+const Icon = styled.div`
+    .icon-container{
+        display: flex;
+        align-items: center;
+    }
+
+    .sun-moon{
+        color: ${({ theme }) => theme === 'dark' ? '#FFFFFF' : '#8A8694'};
+        font-size: 25px;
+        margin-right: 10px;
+    }
+
+    .signIn{
+        font-size: 14px;
+        color: #ffffff;
+        background: none;
+        border: 2px solid transparent;
+        padding: 5px 10px;
+        border-radius: 10px;
+        position: relative;
+        z-index: 1;
+        background: linear-gradient(to right, #ff6b6b, #f06595, #00b4d8, #0077b6);
+    }
+
+    .signIn::before{
+        content: '';
+        position: absolute;
+        top: -2px;
+        left: -2px;
+        right: -2px;
+        bottom: -2px; 
+        border-radius: 10px;
+        background: linear-gradient(to left, #ff6b6b, #f06595, #00b4d8, #0077b6);
+        z-index: -1;
+    }
 `;
 
 const Avatar = styled(Image)`
@@ -55,80 +184,76 @@ const DropdownHeader = styled.div`
   }
 `;
 
-const List = styled(Link)`
-    color: ${({ theme }) => theme === 'dark' ? '#FFFFFF' : 'black'};
-`;
-
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
     const dispatch = useDispatch();
     const { currentUser } = useSelector((state) => state.user);
     const { theme } = useSelector((state) => state.theme);
 
-  return (
-    <StyledNavbar>
-        <Logo to='/' className='align-self-center text-nowrap fw-bolder'>
-            <LogoName>Sahand's</LogoName>
-            <Blog theme={theme}>Blog</Blog>
-        </Logo>
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
-        <form className='d-none d-lg-inline'>
-            <input type="text" placeholder="search..." />
-            <button><FaSearch/></button>
-        </form>
+    return (
+        <HeaderContainer>
+            <NavBar theme={theme}>
+                <Container className="container" isOpen={isOpen} theme={theme}>
+                    <div className="logo">Chamika</div>
 
-        <button className='d-lg-none rounded'><FaSearch/></button>
+                    <form action="">
+                        <div className="search-box">
+                            <InputWrapper className="input-wrapper">
+                                <input type="text" name="search" id="search" className='search' placeholder='Search...' />
+                                <button><FaSearch/></button>
+                            </InputWrapper>
+                        </div>
+                    </form>
 
-        <div className='d-flex gap-3 flex-md-row-reverse'>
+                    <div className="nav">
+                        <li><StyledLink to='/' theme={theme}>Home</StyledLink></li>
+                        <li><StyledLink to='/about' theme={theme}>About</StyledLink></li>
+                        <li><StyledLink to='/projects' theme={theme}>Projects</StyledLink></li>
+                    </div>
 
-            <div className="">
-                <button className='d-none d-sm-inline' onClick={() => dispatch(toggleTheme())}>
-                    { theme === 'light' ? <FaSun/> : <FaMoon/> }
-                </button>
+                    <Icon className="icon" theme={theme}>
+                        <div className="icon-container">
+                            <button className='sun-moon' onClick={() => dispatch(toggleTheme()) }>
+                                { theme === 'light' ? <FaMoon/> : <FaSun/> }
+                            </button>
 
-                {currentUser ? (
-                    <Dropdown>
-                        <Dropdown.Toggle as="div" id="dropdown-custom-components">
-                            <Avatar src={currentUser.profilePicture} alt="user" />
-                        </Dropdown.Toggle>
-                        
-                        <Dropdown.Menu>
+                            {currentUser ? (
+                            <Dropdown>
+                                <Dropdown.Toggle as="div" id="dropdown-custom-components">
+                                    <Avatar src={currentUser.profilePicture} alt="user" />
+                                </Dropdown.Toggle>
+                                
+                                <Dropdown.Menu>
+                                    <DropdownHeader>
+                                        <span className="username">@{currentUser.username}</span>
+                                        <span className="email">{currentUser.email}</span>
+                                    </DropdownHeader>
 
-                            <DropdownHeader>
-                                <span className="username">@{currentUser.username}</span>
-                                <span className="email">{currentUser.email}</span>
-                            </DropdownHeader>
+                                    <Dropdown.Item as={Link} to='/dashboard?tab=profile'>Profile</Dropdown.Item>
+                                    <Dropdown.Item>Sign Out</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            ):
+                            (
+                                <Link to='/sign-in'>
+                                    <button className='signIn'>Sign In</button>
+                                </Link>
+                            )}
+                        </div>
+                    </Icon>
 
-                            <Dropdown.Item as={Link} to='/dashboard?tab=profile'>Profile</Dropdown.Item>
-                            <Dropdown.Item>Sign Out</Dropdown.Item>
-
-                        </Dropdown.Menu>
-                    </Dropdown>
-                ):
-                (
-                    <Link to='/sign-in'>
-                        <button>Sign In</button>
-                    </Link>
-                )}
-
-            </div>
-            
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <List to='/' className="nav-link" theme={theme}>Home</List>
-                </li>
-                <li className="nav-item">
-                    <List to='/about' className="nav-link" theme={theme}>About</List>
-                </li>   
-                <li className="nav-item">
-                    <List to='/projects' className="nav-link" theme={theme}>Projects</List>
-                </li>   
-            </ul>
-
-        </div>
-
-    </StyledNavbar>
-  )
+                    <button theme={theme} className="hamburger" onClick={toggleMenu}>
+                        <FaBars />
+                    </button>
+                </Container>
+            </NavBar>
+        </HeaderContainer>
+    )
 }
 
 export default Header;
