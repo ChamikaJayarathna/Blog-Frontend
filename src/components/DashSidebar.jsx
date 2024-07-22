e3f3d4f1ede12bce01980a287f0f19141b3ed870
Chamika Jayarathna <failgames.lk@gmail.com>
2024-07-22 23:40:58 +0530

    update new sidebar 0.0.1

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -1,63 +1,128 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Link as RouterLink } from 'react-router-dom';
+import { RxDashboard } from "react-icons/rx";
+import { IoIosArrowForward } from "react-icons/io";
+import { useSelector } from 'react-redux';
 
-const SidebarItem = styled.li`
-  :hover{
-    background-color: red;
+const Container = styled.div`
+  height: 100vh;
+  background-color: ${(props) => (props.darkMode ? '#18191a' : '#E4E9F7')};
+  transition: all 0.5s ease;
+`;
+
+const SidebarContainer = styled.nav`
+  position: relative;
+  left: 0;
+  height: 100%;
+  width: ${(props) => (props.close ? '88px' : '250px')};
+  padding: 10px 14px;
+  background-color: ${(props) => (props.darkMode ? '#242526' : '#FFFFFF')};
+  transition: all 0.5s ease;
+  z-index: 100;
+`;
+
+const ToggleButton = styled(IoIosArrowForward)`
+  position: absolute;
+  top: 3%;
+  right: -13px;
+  transform: translateY(-50%) rotate(${(props) => (props.close ? '0' : '180deg')});
+  height: 25px;
+  width: 25px;
+  background-color: ${(props) => (props.darkMode ? '#3A3B3C' : '#695CFE')};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  color: ${(props) => (props.darkMode ? '#CCCCCC' : '#FFFFFF')};
+  font-size: 22px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+`;
+
+const MenuBar = styled.div`
+  height: 10%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-top: 35px;
+`;
+
+const MenuLinks = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const NavLink = styled.li`
+  height: 50px;
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+  border-radius: 6px;
+  transition: all 0.4s ease;
+
+  &:hover{
+    background-color: ${(props) => (props.darkMode ? '#3A3B3C' : '#695CFE')};
+  }
+
+  &:hover .icon,
+  &:hover .text{
+    color: ${(props) => (props.darkMode ? '#CCCCCC' : '#FFFFFF')};
   }
 `;
 
-const StyledLink = styled(Link)`
+const Link = styled(RouterLink)`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
   text-decoration: none;
-  color: inherit;
+  color: ${(props) => (props.darkMode ? '#CCCCCC' : '#707070')};
+  transition: all 0.2s ease;
+
+  .icon{
+    min-width: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+  }
+
+  .text{
+    white-space: nowrap;
+    opacity: ${(props) => (props.close ? '0' : '1')};
+    transition: all 0.4s ease;
+  }
 `;
 
 const DashSidebar = () => {
+  const [isSidebarClosed, setSidebarClosed] = useState(true); 
+  const isDarkMode = useSelector((state) => state.theme.theme === 'dark');
 
-  const location = useLocation();
-  const [tab, setTab] = useState('');
-
-  useEffect(() => {
-      const urlParams = new URLSearchParams(location.search);
-      const tabFromUrl = urlParams.get('tab');
-      if(tabFromUrl){
-      setTab(tabFromUrl);
-      }
-  },[location.search]);
+  const toggleSidebar = () => {
+    setSidebarClosed(prevState => !prevState); 
+  };
 
   return (
-    <div className='container-fluid m-0 p-0'>
-      <div className="row">
-        '<div className="bg-dark col-auto col-md-20 min-vh-100 d-flex justify-content-between flex-column">
-          <div className="">
-            <ul className='nav nav-pills flex-column mt-3 mt-sm-0'>
-
-              <StyledLink to='/dashboard?tab=profile'>
-                <SidebarItem className="nav-item text-white fs-4 my-1 py-2 py-sm-0 mt-3">
-                    <a href="#" className='nav-link text-white fs-5' aria-current="page">
-                      <HiUser/>
-                      <span className='ms-3 d-none d-sm-inline'>Profile</span>
-                    </a>
-                  </SidebarItem>
-              </StyledLink>
-              
-
-              <SidebarItem className="nav-item text-white fs-4 my-1 py-2 py-sm-0">
-                <a href="#" className='nav-link text-white fs-5' aria-current="page">
-                  <HiArrowSmRight/>
-                  <span className='ms-3 d-none d-sm-inline'>Sign Out</span>
-                </a>
-              </SidebarItem>
-
-            </ul>
-          </div>
-        
-        </div>
-      </div>
+    <div>
+      <Container darkMode={isDarkMode}>
+        <SidebarContainer close={isSidebarClosed} darkMode={isDarkMode}>
+          <ToggleButton close={isSidebarClosed} darkMode={isDarkMode} onClick={toggleSidebar} />
+          <MenuBar>
+            <MenuLinks>
+              <NavLink darkMode={isDarkMode}>
+                <Link darkMode={isDarkMode} close={isSidebarClosed}>
+                  <RxDashboard className='icon'/>
+                  <span className="text nav-text">Dashboard</span>
+                </Link>
+              </NavLink>
+            </MenuLinks>
+          </MenuBar>
+        </SidebarContainer>
+      </Container>
     </div>
-  )
+  );
 }
 
 export default DashSidebar;
