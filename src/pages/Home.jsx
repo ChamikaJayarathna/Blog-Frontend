32a4fccb14e638c5039e95f40f6be6e44e698c63
Chamika Jayarathna <failgames.lk@gmail.com>
2024-07-29 23:04:00 +0530

    add homa page on postcard v-0.0.1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import PostCard from '../components/PostCard';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
 
-export default Home
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <div className="d-flex flex-wrap gap-4 mt-5">
+        {posts && posts.length > 0 && (
+          posts.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
