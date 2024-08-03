c67d50a263ad7507093738519f5ee743b85dc09d
Chamika Jayarathna <failgames.lk@gmail.com>
2024-08-03 15:48:32 +0530

    update home page and blog post components 0.0.2-V3

--- src/components/BlogPostsCom/BlogPosts.jsx
+++ src/components/BlogPostsCom/BlogPosts.jsx
@@ -8,12 +8,10 @@ const BlogPosts = ({ post }) => {
       <Link to={`/post/${post.slug}`} className='text-decoration-none'>
         <div className="card">
           <img src={post.images[0]} className="card-img-top" alt={post.title} />
-          <div className="card-body">
-            <h2 className="card-title text-start">{post.title}</h2>
-            <p className="card-text">{post.excerpt}</p>
-            <span className='category'>{post.category}</span>
-          </div>
         </div>
+        <div className="card-body">
+            <p className="card-title text-start">{post.title}</p>
+          </div>
       </Link>
     </div>
   );
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -3,7 +3,6 @@ import BlogPosts from '../../components/BlogPostsCom/BlogPosts';
 import './home.css';
 
 const Home = () => {
-
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -15,22 +14,17 @@ const Home = () => {
     fetchPosts();
   }, []);
 
-  const getGridItemClass = (index) => {
-    const classes = ['tall', 'wide', 'big'];
-    return classes[index % classes.length];
-  };
-
   return (
-    <div className='wrapper'>
-      <div className="grid-wrapper">
-        {posts && posts.length > 0 && posts.map((post, index) => (
-          <div key={post._id} className={`grid-item ${getGridItemClass(index)}`}>
+    <div className="wrapper">
+      <div className="container">
+        {posts.length > 0 && posts.map((post, index) => (
+          <div key={post._id} className="box">
             <BlogPosts post={post} />
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
