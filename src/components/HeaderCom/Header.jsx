f791080ced613096e10c945afdd1b6ab7b7edae1
Chamika Jayarathna <failgames.lk@gmail.com>
2024-08-06 23:03:00 +0530

    upade ui header v0-0.0.8

--- src/components/HeaderCom/Header.jsx
+++ src/components/HeaderCom/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IoSearch } from "react-icons/io5";
 import { FaMoon, FaSun } from 'react-icons/fa';
@@ -10,9 +10,30 @@ import './header.css';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [open, setOpen] = useState(false);
     const theme = useSelector((state) => state.theme.theme);
     const dispatch = useDispatch();
     const currentUser = useSelector((state) => state.user.currentUser);
+    let menuRef = useRef();
+
+
+    useEffect(() => {
+        let handler = (e) => {
+            if(!menuRef.current.contains(e.target)){
+                setOpen(false)
+                
+            }
+        };
+    
+        document.addEventListener("mousedown", handler);
+    
+        return() =>{
+            document.removeEventListener("mousedown", handler);
+        }
+    });
+
+
+
 
     const handleMenuToggle = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -30,7 +51,6 @@ const Header = () => {
             } else {
                 dispatch(signoutSuccess());
             }
-
         } catch (error) {
             console.log(error.message);
         }
@@ -38,39 +58,37 @@ const Header = () => {
 
     return (
         <header className={`header-comp-style`}>
-
             <div className="header-comp-left-side">
                 <Link to="/" className="header-comp-logo">Logo</Link>
-
                 <ul className={`header-comp-navigation ${isMenuOpen ? 'open' : ''}`}>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
-                    <li><Link>Contact Us</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
                 </ul>
             </div>
 
             <div className="header-comp-search-wrapper header-comp-icon">
-                <input type="text" className='header-comp-search-bar-input' placeholder="Search here..."/>
+                <input type="text" className='header-comp-search-bar-input' placeholder="Search here..." />
                 <button className='header-comp-search-bar-submit'>
                     <IoSearch className="header-comp-search-btn" />
                 </button>
             </div>
 
             <div className="header-comp-right-side">
-
                 <div className="header-comp-icon">
                     <button className='sun-moon' onClick={() => dispatch(toggleTheme())}>
                         {theme === 'light' ? <FaSun /> : <FaMoon />}
                     </button>
 
                     {currentUser ? (
-                        <div className="dropdown">
-                            <div className="dropdown-toggle" id="dropdown-custom-components">
+                        <div className="header-comp-menu-container" ref={menuRef}>
+                            <div className="header-comp-menu-trigger" onClick={() => setOpen(!open)}>
                                 <img src={currentUser.profilePicture} alt="user" className="avatar" />
                             </div>
-                            <div className="dropdown-menu">
-                                <div className="dropdown-header">
-                                    <span className="username">@{currentUser.username}</span>
+
+                            <div className={`header-comp-dropdown-menu ${open ? 'active' : 'inactive'}`}>
+                                <div className="header-comp-dropdown-header">
+                                    <span className="username">@{currentUser.username}</span><br />
                                     <span className="email">{currentUser.email}</span>
                                 </div>
                                 <Link to='/dashboard?tab=profile' className="dropdown-item">Profile</Link>
@@ -83,7 +101,6 @@ const Header = () => {
                         </Link>
                     )}
                 </div>
-
                 <IoMdMenu className='menu-toggle' onClick={handleMenuToggle} />
             </div>
         </header>
